2ad4f85d7885721a7cb7d49c7dcbdcee189276a1
jjaryupwork <jjaryupwork@gmail.com>
2017-09-12 20:43:56 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 jQuery(document).ready(function(){
     $(document).on('click', 'button', function() {
-        $(this).next("span").toggleClass("hidden");
+            $(this).next("span").toggleClass("hidden");
+        });
+
     });
-});
 
 jQuery(document).ready(function($){
-    $("a").click(function(event){
+        $(document).on('click', 'a', function() {  
         link=$(this).attr("href");
         $.ajax({
             url: link
@@ -14,33 +15,6 @@ jQuery(document).ready(function($){
             .done(function(html){
 
                 $("#card").remove();
-                $("#page").empty().append(html);
-
-            })
-            .fail(function(){
-                console.log("error");
-
-            })
-            .always(function(){
-                console.log("complete");
-            });
-
-        return false;
-
-    });
-});
-
-
-jQuery(document).ready(function($){
-    $("a").click(function(event){
-        link=$(this).attr("href");
-        $.ajax({
-            url: link
-        })
-
-            .done(function(html){
-
-                $( ".card" ).remove();
                 $("#page").empty().append(html);
 
             })
